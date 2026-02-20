b0ee3d54e843941a47501987c5a81b763500d4d8
AleksandarHaralampiev <alexandar.h.haralampiev@gmail.com>
2026-02-20 21:10:44 +0200

    alek commited calc

---
+++ calc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+   int a, b;
+   int sum;
+
+
+   printf("Vavedi dve chisla: ");
+   scanf("%d %d", &a, &b);
+
+
+   sum = a + b;
+
+
+   printf("Sborat e: %d\n", sum);
+
+
+   return 0;
+}
